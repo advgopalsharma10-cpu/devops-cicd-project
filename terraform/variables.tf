c755f6744867b881adfd30174749aaa5dca7a6f3
advgopalsharma10-cpu <advgopalsharma10@gmail.com>
2026-06-05 01:45:21 +0530

    Change EC2 instance type to t3.micro

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "ec2_key_name" {
